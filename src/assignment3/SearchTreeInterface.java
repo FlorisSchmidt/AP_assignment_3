2d875c3cdce271c19825dd35d84f6b7ac81dcdb6
falksinke <falksinke@gmail.com>
2019-10-15 20:30:04 +0200

    Completed SearchTreeInterface and IdentifierInterface

--- src/assignment3/SearchTreeInterface.java
+++ src/assignment3/SearchTreeInterface.java
@@ -3,47 +3,76 @@ import java.util.Iterator;
 
 public interface SearchTreeInterface<E extends Comparable<E>> {
 
-    /**
-    @postcondition
-    The data stored in the binary search tree was iterated in
-    monotonically non-decreasing order and was added in this
-    order to an object of the type Iterator<E>.
-    This  object  of  the type Iterator<E>was  subsequently
-    returned.
+    /** @elements
+     *  All elements of type E extending the comparable class
+     *  @structure
+     *    Left subtree of a node contains only nodes with values lesser than the node's key.
+     *    Right subtree of a node contains only nodes with values greater than the node's key.
+     *  @domain
+     *    Only alphanumeric characters. Identifiers begins with a letter.
+     *    Identifier has a length at least 1 character
+     *  @constructor
+     *    There is a default constructor that creates an empty tree
+     *  @precondition
+     *    --
+     *  @postcondition
+     *    The new SearchTree-object contains an empty tree
+     *
+     **/
+
+
+    /** @precondition
+     * --
+     * @postcondition
+     * The data stored in the binary search tree was iterated in
+     * monotonically non-decreasing order and was added in this
+     * order to an object of the type Iterator<E>.
+     * This  object  of  the type Iterator<E>was  subsequently
+     * returned.
      **/
     Iterator<E>ascendingIterator ();
 
-    /**
-    @postcondition
-    The data stored in the binary search tree was iterated in
-    monotonically non-increasing order and was added in this
-    order to an object of the type Iterator<E>.
-    This object of the type Iterator<E>was subsequently
-    returned.**/
+    /** @precondition
+     * --
+     * @postcondition
+     *The data stored in the binary search tree was iterated in
+     * monotonically non-increasing order and was added in this
+     * order to an object of the type Iterator<E>.
+     * This object of the type Iterator<E>was subsequently
+     * returned.
+     **/
     Iterator<E>descendingIterator ();
 
-    /**
-     @precondition
-     @postcondition
+
+    /** @precondition
+     * --
+     * @postcondition
+     * SearchTree-POST is empty and root its root points to null.
      **/
     void init();
 
 
-    /**
-     @precondition
-     @postcondition
+    /** @precondition
+     * --
+     * @postcondition
+     * SearchTree-POST contains the value passed as argument.
      **/
     void insert(E value);
 
-    /**
-     @precondition
-     @postcondition
+    /** @precondition
+     * The SearchTree is not empty.
+     * @postcondition
+     * SearchTree-POST does not contain the value.
+     * @return  true: SearchTree-PRE contains the value passed as argument and has now been deleted.
+     *          false: SearchTree-PRE does not contain the value.
      **/
     boolean delete(E value);
 
-    /**
-     @precondition
-     @postcondition
+    /** @precondition
+     * The SearchTree is not empty.
+     * @postcondition
+     * @return  true: SearchTree contains the value passed as argument.
+     *          false: SearchTree does not contain the value passed as argument.
      **/
     boolean find(E value);
 }
