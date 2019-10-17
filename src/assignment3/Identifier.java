abd8840c64591d7e1bb3ca965783cdae992df2b7
floris.schmidt <floris.schmidt@hotmail.com>
2019-10-17 16:54:43 +0200

    Removed redundant testDomain method

--- src/assignment3/Identifier.java
+++ src/assignment3/Identifier.java
@@ -1,7 +1,6 @@
 package assignment3;
 
 import java.lang.StringBuffer;
-import java.lang.Character;
 
 public class Identifier implements IdentifierInterface, Comparable<Identifier> {
 
@@ -28,25 +27,15 @@ public class Identifier implements IdentifierInterface, Comparable<Identifier> {
     }
 
     @Override
-    public boolean add(char c) {
-        if(testDomain(c) && addWasCalled) {
+    public boolean add(char c){
+        if(addWasCalled) {
             sb.append(c);
-            return true;
-        }
-        if(testDomain(c) && !addWasCalled) {
+        } else {
             sb.delete(0, 1);
             sb.append(c);
             addWasCalled = true;
-            return true;
         }
-        return false;
-    }
-
-    private boolean testDomain(char c) {
-        if(!addWasCalled && Character.isDigit(c)) {
-            return false;
-        }
-        return Character.isDigit(c) || Character.isLowerCase(c) || Character.isUpperCase(c);
+        return true;
     }
 
     @Override
@@ -61,11 +50,12 @@ public class Identifier implements IdentifierInterface, Comparable<Identifier> {
 
     @Override
     public boolean equals(Identifier comparand) {
-        return value().equals(comparand.value());
+        return this.value().equals(comparand.value());
     }
 
     @Override
     public int compareTo(Identifier o) {
         return value().compareTo(o.value());
     }
+
 }
